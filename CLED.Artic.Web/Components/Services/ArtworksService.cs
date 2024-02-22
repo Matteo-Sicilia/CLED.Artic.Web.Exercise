6ee6cbff5b5c09994ca0088d00dbe702fffd9be4
Matteo Sicilia <150261032+Matteo-Sicilia@users.noreply.github.com>
2024-02-22 16:50:53 +0100

    Add project files.

---
+++ CLED.Artic.Web/Components/Services/ArtworksService.cs
@@ -0,0 +1,26 @@
+﻿namespace CLED.Artic.Web.Components.Services;
+using CLED.Artic.Web.Models;
+using static CLED.Artic.Web.Models.GetArtworksResponse;
+
+public class ArtworksService
+{
+
+    private readonly HttpClient _httpClient;
+
+    public ArtworksService(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+    public async Task<IEnumerable<Artwork>> GetArtworks()
+    {
+        var response = await _httpClient.GetFromJsonAsync<GetArtworksResponse>("https://api.artic.edu/api/v1/artworks");
+
+        if (response == null)
+            return [];
+
+        return response.Data;
+     
+    }
+
+
+}
